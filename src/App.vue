570430ab355271f693f6c8163c5ce9cb45156e15
Yozora_cyx <915658157@qq.com>
2019-07-18 14:11:04 +0800

    九宫格布局，TabBar切换动画

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <mt-header fixed title="Yozora-vue"></mt-header>
 
     <!-- 中间 路由 router-view 区域 -->
-    <router-view></router-view>
+    <transition>
+      <router-view></router-view>
+    </transition>
 
     <!-- 底部 Tabbar 区域 -->
     <nav class="mui-bar mui-bar-tab">
@@ -37,5 +39,22 @@
 <style lang="scss" scoped>
 .app-container {
   padding-top: 40px;
+  overflow-x: hidden;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 //3. 创建路由对象
 var router = new VueRouter({
   routes: [ //配置路由规则
+    { path: '/', redirect: '/home' },
     { path: '/home', component: HomeContainer },
     { path: '/member', component: MemberContainer },
     { path: '/search', component: SearchContainer },
